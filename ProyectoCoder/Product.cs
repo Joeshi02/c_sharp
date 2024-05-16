e2f59692d1d7f4c6f38f0da3ce60da2566b249b4
Joeshi02 <joaco.escobar.h@gmail.com>
2024-05-16 02:23:13 -0300

    primera pre entrega

---
+++ ProyectoCoder/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCoder
+{
+    public class Product
+    {
+        private int _codigo;
+        private string _description;
+        private double _precioDeCompra;
+        private double _precioDeVenta;
+        private string _categoria; 
+
+        public Product()
+        {
+            _codigo = 0;
+            _description = string.Empty;
+            _precioDeCompra= 0;
+            _precioDeVenta = 0;
+            _categoria= string.Empty
+        }
+    }
+}
